870d786c99c26623a28ec4c874e0b3c61810afea
AkaakuHub <akaaku2021media01@gmail.com>
2026-05-10 12:10:59 +0900

    feat: 証明書を設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,27 @@
 import { defineConfig, loadEnv } from 'vite'
+import { readFileSync } from 'node:fs'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const backendTarget = env.VITE_BACKEND_URL;
+  const currentDir = dirname(fileURLToPath(import.meta.url));
+  const certDir = resolve(currentDir, "..");
 
   // chrome prompt apiのため、httpsを強制
   return {
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     server: {
       host: '0.0.0.0', // 外部からのアクセスを許可
       port: 5173,
-      https: {},
+      https: {
+        key: readFileSync(resolve(certDir, "nijigasaki+2-key.pem")),
+        cert: readFileSync(resolve(certDir, "nijigasaki+2.pem")),
+      },
       fs: {
         allow: ['..', '../..'] // プロジェクトルート外のアクセスを許可
       },
